2e3613c76515da66309dddcaadc19a82c7293d6a
Hansh31 <anilchinna13@gmail.com>
2025-01-05 05:16:39 +0530

    new message

--- 01-Palindrome.sh
+++ 01-Palindrome.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Enter a string:"
-read str
-rev=$(expr $str|rev)
-if [ $rev=$str ]
-then
-echo "the given string is palindrome "
+# echo "Enter a string:"
+# read str
+# rev=$($str )
+# if [ $rev=$str ]
+# then
+# echo "the given string is palindrome "
+# else
+# echo "the given string is not palindrome "
+# fi
+
+read -p "Enter a string: " input_string
+
+
+
+# Reverse the string and compare with the original
+
+
+
+if [[ "$input_string" == "$(rev <<< "$input_string")" ]]; then
+    echo "The entered string '$input_string' is a palindrome."
 else
-echo "the given string is not palindrome "
+    echo "The entered string '$input_string' is not a palindrome."
 fi
